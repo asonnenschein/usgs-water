01a61a1201e8f9e8de131cf4ea9280e876ff32d0
asonnenschein <adrian.sonnenschein@gmail.com>
2014-10-18 13:43:04 -0700

    http route for harvesting and reducing

--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,16 @@
-var mocha = require('mocha')
-  , supertest = require('supertest')
+var supertest = require('supertest')
   , server = require('./../lib/server')
   ;
 
 describe('Tests', function () {
 
-  describe('Harvest USGS JSON for all 50 states to GeoJSON', function (done) {
-    var req = {
-
-    };
-
-    supertest(server)
-      .post()
-      .send(req)
-      .expect(200, done)
-  })
+  describe('Harvest USGS JSON for all 50 states to GeoJSON', function () {
+    this.timeout(150000000);
+    it('should return 200 when posting to the server', function (done) {
+      supertest(server)
+        .post('/usgs-water/harvest')
+        .expect(200, done)
+    })
+  });
 
 });
