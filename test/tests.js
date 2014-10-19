e785bb3b34df5aa6c67effb6bcc6b5dd9a051094
asonnenschein <adrian.sonnenschein@gmail.com>
2014-10-19 09:21:57 -0700

    return geojson to client

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,7 @@ var supertest = require('supertest')
   ;
 
 describe('Tests', function () {
-
+/*
   describe('Harvest USGS JSON for all 50 states to GeoJSON', function () {
     this.timeout(150000000);
     it('should return 200 when posting to the server', function (done) {
@@ -12,5 +12,12 @@ describe('Tests', function () {
         .expect(200, done)
     })
   });
-
+*/
+  describe('Get reduced GeoJson object from the database', function () {
+    it('should return 200 when getting from the server', function (done) {
+      supertest(server)
+        .get('/usgs-water/data')
+        .expect(200, done)
+    })
+  })
 });
